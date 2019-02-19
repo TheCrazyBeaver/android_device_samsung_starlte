10c4d657a7cc5c57cd0992ef3478a787137188b7
TheCrazyBeaver <47144753+TheCrazyBeaver@users.noreply.github.com>
2019-02-19 11:16:52 +0100

    Update aosp_starlte.mk

--- aosp_starlte.mk
+++ aosp_starlte.mk
@@ -7,4 +7,9 @@ $(call inherit-product, device/samsung/starlte/full_starlte.mk)
 $(call inherit-product, vendor/aosp/config/common.mk)
 $(call inherit-product, vendor/aosp/config/telephony.mk)
 
+BUILD_FINGERPRINT := samsung/starltexx/starlte:8.0.0/R16NW/G960FXXU1ARCC:user/release-keys
+
+ PRODUCT_BUILD_PROP_OVERRIDES += \
+        PRIVATE_BUILD_DESC="starltexx-user 8.0.0 R16NW G960FXXU1ARCC release-keys"
+        
 PRODUCT_NAME := aosp_starlte
